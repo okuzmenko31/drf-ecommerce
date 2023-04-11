ac69235f083fd475010e81be70148d12e230a57a
okuzmenko31 <olegkuzmenko2005@gmail.com>
2023-04-11 19:43:45 +0300

    Added products, categories and users applications

--- ecommerce/core/settings.py
+++ ecommerce/core/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken'
+
+    # applications
+    'categories'
 ]
 
 MIDDLEWARE = [
