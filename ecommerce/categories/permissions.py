3563c179371ce5ae09fc7702f67af1cfa0a90c3d
okuzmenko31 <olegkuzmenko2005@gmail.com>
2023-04-23 16:50:59 +0300

    Added tests and permission for categories.

--- ecommerce/categories/permissions.py
+++ ecommerce/categories/permissions.py
@@ -8,6 +8,11 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        if request in permissions.SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_staff
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
             return True
         return bool(request.user and request.user.is_staff)
--- ecommerce/categories/views.py
+++ ecommerce/categories/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from .models import Category
